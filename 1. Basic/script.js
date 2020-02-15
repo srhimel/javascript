7e5b70380a3b2786ac0374b258e8d9c7ba020b6e
shahamar rahman <himusr@gmail.com>
2020-02-15 23:39:43 +0600

    learned console prompt and alert

---
+++ 1. Basic/script.js
@@ -0,0 +1,4 @@
+var firstName = prompt("What is your first name ?");
+var lastName = prompt("What is your last name?");
+console.log(firstName + ' ' + lastName);
+alert("Your name is"+ ' '+ firstName + ' ' + lastName);
