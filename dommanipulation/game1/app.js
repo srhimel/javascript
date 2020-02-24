4c24b41106ac8847884e20bb1eca9ca00634c3ea
shahamar rahman <himusr@gmail.com>
2020-02-24 20:43:51 +0600

    play the game

--- dommanipulation/game1/app.js
+++ dommanipulation/game1/app.js
@@ -8,13 +8,6 @@ GAME RULES:
 - The first player to reach 100 points on GLOBAL score wins the game
 
 */
-alert('GAME RULES:'+
-
-'- The game has 2 players, playing in rounds'+
-'- In each turn, a player rolls a dice as many times as he whishes. Each result get added to his ROUND score'+
-'- BUT, if the player rolls a 1, all his ROUND score gets lost. After that, its the next players turn'+
-'- The player can choose to Hold, which means that his ROUND score gets added to his GLBAL score. After that, its the next players turn'+
-'- The first player to reach 100 points on GLOBAL score wins the game')
 
 var scores, roundScore, activePlayer,player1,player2,gamePlaying;
 init();
